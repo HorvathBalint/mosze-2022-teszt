e4e527a41f4e2f838d61bba42efd7b11ce91c91a
HorvathBalint <balint0611424@gmail.com>
2022-09-19 14:55:48 +0200

    kommentek

--- main.cpp
+++ main.cpp
@@ -4,19 +4,19 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
+    int *b = new int[NELEMENTS];// A változó nem létezik.
+    std::cout << '1-100 ertekek duplazasa'//lemaradt a  pontos vessző  és az endl a sor végéről.
     for (int i = 0;)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)//rossz a for ciklus.
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"//nincs pontos vessző a sor végén.
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;//átlagnak érdemes double típusúnak lenni.
+    for (int i = 0; i < N_ELEMENTS, i++)//N_ELEMENTS után pontos vessző szükséges.
     {
         atlag += b[i]
     }
