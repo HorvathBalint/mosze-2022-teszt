86976b8157ef6b67be1b02efcc0a6d9194fd18e6
HorvathBalint <balint0611424@gmail.com>
2022-09-19 15:41:59 +0200

    javított kód

--- main.cpp
+++ main.cpp
@@ -4,21 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];// A változó nem létezik.
-    std::cout << '1-100 ertekek duplazasa'//lemaradt a  pontos vessző  és az endl a sor végéről.
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS];                                // A változó nem létezik.
+    std::cout << '1-100 ertekek duplazasa'<<std::endl;           //lemaradt a  pontos vessző  és az endl a sor végéről.
+    for (int i = 0;i<N_ELEMENTS;i++)                                             //rossz for ciklus
     {
-        b[i] = i * 2;
+        b[i] = (i+1) * 2;
     }
-    for (int i = 0; i; i++)//rossz a for ciklus.
+    std::cout << "Ertek:";                                    //nincs pontos vessző a sor végén.
+    for (int i = 0; i<N_ELEMENTS; i++)                             //rossz a for ciklus.
     {
-        std::cout << "Ertek:"//nincs pontos vessző a sor végén.
-    }    
+        std::cout<<b[i]<<' ';
+    }
+    std::cout<<std::endl;    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;//átlagnak érdemes double típusúnak lenni.
-    for (int i = 0; i < N_ELEMENTS, i++)//N_ELEMENTS után pontos vessző szükséges.
+    double atlag;                                                //átlagnak érdemes double típusúnak lenni.
+    for (int i = 0; i < N_ELEMENTS; i++)                         //N_ELEMENTS után pontos vessző szükséges.
     {
-        atlag += b[i]
+        atlag += b[i];                                           //pontos vessző hiánya
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
